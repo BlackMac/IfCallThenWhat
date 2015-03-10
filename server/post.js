0d7f390182d58cc10290479c8bbb411023d2cc48
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-03-10 08:26:50 +0100

    initial commit

---
+++ server/post.js
@@ -0,0 +1,9 @@
+parsePost = function(input) {
+  var result = {};
+  var parts = input.split("&");
+  parts.forEach(function(part) {
+    var divided = part.split("=");
+    result[divided[0]] = divided[1];
+  });
+  return result;
+}
